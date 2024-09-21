8441ae308d1b3e245d8e9a0687b64c2b2a952060
Mohamad-Ammis <esd676q8@gmail.com>
2024-09-21 15:40:54 -0700

    create serve failure from dio

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,56 @@
-abstract class Faliure {}
+import 'package:dio/dio.dart';
 
-class ServerFaliure extends Faliure {}
+abstract class Faliure {
+  final String errorMessage;
+
+  Faliure({required this.errorMessage});
+}
+
+class ServerFaliure extends Faliure {
+  ServerFaliure({required super.errorMessage});
+  factory ServerFaliure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFaliure(
+            errorMessage: 'Connection timeout with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFaliure(errorMessage: 'Send timeout with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFaliure(errorMessage: 'Receive timeout with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFaliure(errorMessage: 'bad Certificate with api server');
+
+      case DioExceptionType.badResponse:
+        return ServerFaliure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFaliure(errorMessage: 'cancel with api server');
+
+      case DioExceptionType.connectionError:
+        return ServerFaliure(errorMessage: 'connectionError with api server');
+
+      case DioExceptionType.unknown:
+        if (dioException.message != null
+            ? dioException.message!.contains('SocketException')
+            : false) {
+          return ServerFaliure(errorMessage: 'No Internet Connection');
+        }
+        return ServerFaliure(errorMessage: 'unexpected Error ,try again');
+      default:
+        return ServerFaliure(errorMessage: 'unexpected Error ,try again');
+    }
+  }
+  factory ServerFaliure.fromResponse(int statusCode, dynamic resposne) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFaliure(errorMessage: resposne['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFaliure(errorMessage: "request not found");
+    } else if (statusCode == 500) {
+      return ServerFaliure(
+          errorMessage: "internal Server Error, please try again later !");
+    } else {
+      return ServerFaliure(
+          errorMessage: "Oops there was an error, please try again later !");
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,10 @@ class HomeRepoImplementation implements HomeRepo {
       }
       return right(books);
     } on Exception catch (e) {
-      return Left(ServerFaliure());
+      if (e is DioException) {
+        return Left(ServerFaliure.fromDioException(e));
+      } 
+      return Left(ServerFaliure(errorMessage: e.toString()));
     }
   }
 
